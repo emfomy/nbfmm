5826bb72ff8f406dcbebbc2770a384cbb9d6e3e1
Mu Yang <emfomy@gmail.com>
2016-06-01 14:34:43 +0800

    Create config files.

---
+++ demo/demo.cpp
@@ -0,0 +1,24 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @file    demo/demo.cpp
+/// @brief   The demo code
+///
+/// @author  Mu Yang <emfomy@gmail.com>
+///
+
+#include <iostream>
+#include <nbfmm.hpp>
+
+using namespace std;
+using namespace nbfmm;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Main function
+///
+int main( int argc, char *argv[] ) {
+  cout << "NBFMM "
+       << NBFMM_VERSION_MAJOR_STRING << "."
+       << NBFMM_VERSION_MINOR_STRING << "."
+       << NBFMM_VERSION_PATCH_STRING << " demo" << endl;
+
+  return 0;
+}
---
+++ include/nbfmm.hpp
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @file    nbfmm.hpp
+/// @brief   The main NBFMM header
+///
+/// @author  Mu Yang <emfomy@gmail.com>
+///
+
+#ifndef NBFMM_HPP_
+#define NBFMM_HPP_
+
+#include <nbfmm/config.hpp>
+
+#endif  // NBFMM_HPP_
