c538b1e17796cfdf46ebeed5042cd81b06687ac7
Mu Yang <emfomy@gmail.com>
2016-06-03 15:17:13 +0800

    Bug fix: Add tailing underscore character to members.

--- source/nbfmm/solver.cu
+++ source/nbfmm/solver.cu
@@ -21,26 +21,26 @@ Solver::Solver(
     max_num_particle_(max_num_particle),
     position_limits_(position_limits),
     kernel_function_(kernel_function) {
-  cudaMalloc(&gpuptr_position, max_num_particle_ * sizeof(float2));
-  cudaMalloc(&gpuptr_effect,   max_num_particle_ * sizeof(float2));
-  cudaMalloc(&gpuptr_weight,   max_num_particle_ * sizeof(float));
-  cudaMalloc(&gpuptr_index,    max_num_particle_ * sizeof(int));
-  cudaMalloc(&gpuptr_perm,     max_num_particle_ * sizeof(int));
-  cudaMalloc(&gpuptr_head,     (base_size_*base_size_+1) * sizeof(int));
-  cudaMalloc3D(&pitchedptr_multipole, make_cudaExtent(base_size_*sizeof(float),  base_size_, num_level_));
-  cudaMalloc3D(&pitchedptr_local,     make_cudaExtent(base_size_*sizeof(float2), base_size_, num_level_));
+  cudaMalloc(&gpuptr_position_, max_num_particle_ * sizeof(float2));
+  cudaMalloc(&gpuptr_effect_,   max_num_particle_ * sizeof(float2));
+  cudaMalloc(&gpuptr_weight_,   max_num_particle_ * sizeof(float));
+  cudaMalloc(&gpuptr_index_,    max_num_particle_ * sizeof(int));
+  cudaMalloc(&gpuptr_perm_,     max_num_particle_ * sizeof(int));
+  cudaMalloc(&gpuptr_head_,     (base_size_*base_size_+1) * sizeof(int));
+  cudaMalloc3D(&pitchedptr_multipole_, make_cudaExtent(base_size_*sizeof(float),  base_size_, num_level_));
+  cudaMalloc3D(&pitchedptr_local_,     make_cudaExtent(base_size_*sizeof(float2), base_size_, num_level_));
 }
 
 // Default destructor
 Solver::~Solver() {
-  cudaFree(gpuptr_position);
-  cudaFree(gpuptr_effect);
-  cudaFree(gpuptr_weight);
-  cudaFree(gpuptr_index);
-  cudaFree(gpuptr_perm);
-  cudaFree(gpuptr_head);
-  cudaFree(gpuptr_multipole);
-  cudaFree(gpuptr_local);
+  cudaFree(gpuptr_position_);
+  cudaFree(gpuptr_effect_);
+  cudaFree(gpuptr_weight_);
+  cudaFree(gpuptr_index_);
+  cudaFree(gpuptr_perm_);
+  cudaFree(gpuptr_head_);
+  cudaFree(gpuptr_multipole_);
+  cudaFree(gpuptr_local_);
 }
 
 }  // namespace nbfmm
