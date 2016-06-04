32d2e00c7523b1d2ee1a9674f4c4cea3ce1bb423
blasteg <blasteg@gmail.com>
2016-06-04 16:08:37 +0800

    debugging float to const float when needed

--- source/nbfmm/solver/solve.cu
+++ source/nbfmm/solver/solve.cu
@@ -7,17 +7,21 @@
 #include <math.h>
 #include <cuda_runtime.h>
 #include <nbfmm/solver.hpp>
+#include <thrust/sequence.h>
+#include <thrust/device_ptr.h>
+#include <thrust/scan.h>
+#include <thrust/sort.h>
 
-__global__ void partical_find_grid(int num_particle,float left_bound,float down_bound,float gridWidth,float gridHeight,int base_size,float2* gpuptr_position_origin,int* gpuptr_sortingIndex)
+__global__ void partical_find_grid(int num_particle,float left_bound,float down_bound,float gridWidth,float gridHeight,int base_size,const float2* gpuptr_position_origin,int* gpuptr_sortingIndex)
 {
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 	if (idx>=num_particle)
 		return;
-	int posx=floorf((gpuptr_position[idx].x-left_bound)/gridWidth);
-	int posy=floorf((gpuptr_position[idx].y-down_bound)/gridHeight);
+	int posx=floorf((gpuptr_position_origin[idx].x-left_bound)/gridWidth);
+	int posy=floorf((gpuptr_position_origin[idx].y-down_bound)/gridHeight);
 	gpuptr_sortingIndex[idx]=posy*base_size+posx;
 }
-__global__ void sorting_input(int num_particle,int* gpuptr_perm_,float* gpuptr_position_origin,float* gpuptr_weight_origin,float* gpuptr_position_,float* gpuptr_weight_,int* gpuptr_sortingIndex,int2* gpuptr_index,int base_size)
+__global__ void sorting_input(int num_particle,int* gpuptr_perm_,const float2* gpuptr_position_origin,const float* gpuptr_weight_origin,float2* gpuptr_position_,float* gpuptr_weight_,int* gpuptr_sortingIndex,int2* gpuptr_index,int base_size)
 {
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 	if (idx>=num_particle)
@@ -62,10 +66,10 @@ void Solver::solve(
 	cudaMalloc(&gpuptr_sortingIndex,num_particle*sizeof(int));
 	partical_find_grid<<<KERNEL_gridSize_pointwise,KERNEL_blockSize_pointwise>>>(num_particle,position_limits_.x,position_limits_.y,gridWidth,gridHeight,base_size_, gpuptr_position_origin,gpuptr_sortingIndex);
   thrust::device_ptr<int> trst_permu(gpuptr_perm_),trst_sortingIndex(gpuptr_sortingIndex); //step 2 start
-	thrust::sequence(gpuptr_perm_,gpuptr_perm_+num_particle);
+	thrust::sequence( trst_permu, trst_permu+num_particle);
 
-	thrust::sort_by_key(gpuptr_perm_,gpuptr_perm_+num_particle, trst_sortingIndex);
-	sorting_input<<<KERNEL_gridSize_pointwise,KERNEL_blockSize_pointwise>>>(num_particle,gpuptr_perm_,gpuptr_position_origin,gpuptr_weight_origin, gpuptr_position_,gpuptr_weight_,gpuptr_sortingIndex,gpuptr_index,base_size_);
+	thrust::sort_by_key( trst_permu, trst_permu+num_particle, trst_sortingIndex);
+	sorting_input<<<KERNEL_gridSize_pointwise,KERNEL_blockSize_pointwise>>>(num_particle,gpuptr_perm_,gpuptr_position_origin,gpuptr_weight_origin, gpuptr_position_,gpuptr_weight_,gpuptr_sortingIndex,gpuptr_index_,base_size_);
 	extract_head<<<KERNEL_gridSize_pointwise,KERNEL_blockSize_pointwise>>>(num_particle,gpuptr_sortingIndex,gpuptr_head_); //step3: fill array of starting index
   /// @todo Implement!
 }
