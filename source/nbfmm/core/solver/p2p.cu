3daf2baf70106a101b48c3e73f9fee868603c5f2
Mu Yang <emfomy@gmail.com>
2016-06-12 18:37:18 +0800

    Small changes in P2P.

--- source/nbfmm/core/solver/p2p.cu
+++ source/nbfmm/core/solver/p2p.cu
@@ -12,18 +12,18 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// Compute particle to particle
 ///
-/// @param[in]   num_particle    the number of particles.
-/// @param[in]   cell_side_size  the number of cells in the base level per side.
-/// @param[in]   position        the particle positions.
-/// @param[in]   weight          the particle weights.
-/// @param[in]   index           the particle cell indices.
-/// @param[in]   head            the starting permutation indices of each cell.
-/// @param[out]  effect          the particle effects.
+/// @param[in]   num_particle  the number of particles.
+/// @param[in]   base_dim      the number of cells in the base level per side.
+/// @param[in]   position      the particle positions.
+/// @param[in]   weight        the particle weights.
+/// @param[in]   index         the particle cell indices.
+/// @param[in]   head          the starting permutation indices of each cell.
+/// @param[out]  effect        the particle effects.
 ///
 __global__
 void NaiveP2P(
   const int     num_particle,
-  const int     cell_side_size,
+  const int     base_dim,
   const float2* position,
   const float*  weight,
   const int2*   index,
@@ -34,25 +34,24 @@ void NaiveP2P(
   if ( idx >= num_particle ) {
     return;
   }
-  float2 total_effect = make_float2(0.0f, 0.0f);
-  int2 par_idx = index[idx];
+  const int2   parent_idx    = index[idx];
+  const float2 self_position = position[idx];
+  float2       total_effect  = make_float2(0.0f, 0.0f);
 
-  float2 self_position = position[idx];
-
-  // Go through each surrounding cell
-  for ( int j = par_idx.y-1; j <= par_idx.y+1; ++j ) {
-    for ( int i = par_idx.x-1; i <= par_idx.x+1; ++i ) {
+  // Go through neighbor cells
+  for ( int j = parent_idx.y-1; j <= parent_idx.y+1; ++j ) {
+    for ( int i = parent_idx.x-1; i <= parent_idx.x+1; ++i ) {
 
       // Check whether this cell exists
-      if( i >= 0 && i < cell_side_size && j >= 0 && j < cell_side_size ) {
+      if( i >= 0 && i < base_dim && j >= 0 && j < base_dim ) {
 
-        // Go through each particle in this cell
-        int cell_idx  = par_idx.x + par_idx.y * cell_side_size;
+        // Go through particles in this cell
+        int cell_idx  = parent_idx.x + parent_idx.y * base_dim;
         int start_idx = head[cell_idx];
         int end_idx   = head[cell_idx + 1];
-        for(int k = start_idx; k < end_idx; ++k) {
+        for ( int k = start_idx; k < end_idx; ++k ) {
           // Cannot calculate action to self
-          if(k != idx) {
+          if ( k != idx ) {
             total_effect += nbfmm::kernelFunction(self_position, position[k], weight[k]);
           }
         }
@@ -67,11 +66,11 @@ namespace nbfmm {
 
 // P2P
 void Solver::p2p( const int num_particle ) {
-  const int block_size = kMaxBlockDim;
-  const int num_block = num_particle/block_size + 1;
+  const int block_dim = kMaxBlockDim;
+  const int grid_dim  = ((num_particle-1)/block_dim)+1;
 
-  NaiveP2P<<<num_block, block_size>>>(num_particle, base_dim_,
-                                      gpuptr_position_, gpuptr_weight_, gpuptr_index_, gpuptr_head_, gpuptr_effect_);
+  NaiveP2P<<<grid_dim, block_dim>>>(num_particle, base_dim_,
+                                    gpuptr_position_, gpuptr_weight_, gpuptr_index_, gpuptr_head_, gpuptr_effect_);
 }
 
 }  // namespace nbfmm
