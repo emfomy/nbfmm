4d618d3a13dc5fec7546ec7a2c49f4bf4c327984
blasteg <blasteg@gmail.com>
2016-06-12 20:43:23 +0800

    p2m fix: fix bug when empty cell

--- source/nbfmm/core/solver/p2m.cu
+++ source/nbfmm/core/solver/p2m.cu
@@ -35,15 +35,23 @@ __global__ void p2m_weighting( int num_particle, float2* gpuptr_position, float*
 /// @param[out]  gpuptr_cell_position  the cell positions.
 /// @param[in]   gpuptr_cell_weight    the cell positions.
 ///
-__global__ void p2m_averaging( int base_size, float2* gpuptr_cell_position, float* gpuptr_cell_weight ) {
+__global__ void p2m_assigning( int base_size, float2* gpuptr_cell_position, float* gpuptr_cell_weight,int assigning_length,int2* p2m_assigningIndex ) {
   int thread2Dpx = blockIdx.x * blockDim.x + threadIdx.x;
   int thread2Dpy = blockIdx.y * blockDim.y + threadIdx.y;
   if (thread2Dpx >= base_size || thread2Dpy >= base_size)
    return;
   int thread1Dp = thread2Dpy * base_size + thread2Dpx;
 
-  gpuptr_cell_position[thread1Dp].x=gpuptr_cell_position[thread1Dp].x/gpuptr_cell_weight[thread1Dp];
-  gpuptr_cell_position[thread1Dp].y=gpuptr_cell_position[thread1Dp].y/gpuptr_cell_weight[thread1Dp];
+  gpuptr_cell_position[thread1Dp].x=0;
+  gpuptr_cell_position[thread1Dp].y=0;
+  gpuptr_cell_weight[thread1Dp]=0;
+
+  if (thread1Dp>=assigning_length)
+    return;
+  int index_to_assign=p2m_assigningIndex[thread1Dp].y * base_size +p2m_assigningIndex[thread1Dp].x;
+  gpuptr_cell_position[index_to_assign].x=gpuptr_cell_position[base_size*base_size+thread1Dp].x/gpuptr_cell_weight[base_size*base_size+thread1Dp];
+  gpuptr_cell_position[index_to_assign].y=gpuptr_cell_position[base_size*base_size+thread1Dp].y/gpuptr_cell_weight[base_size*base_size+thread1Dp];
+  gpuptr_cell_weight[index_to_assign]=gpuptr_cell_weight[base_size*base_size+thread1Dp];
 }
 
 //  The namespace NBFMM
@@ -59,23 +67,27 @@ void Solver::p2m( const int num_particle ) {
   float2* p2m_buffer;
   int2* p2m_workingspace;
   cudaMalloc(&p2m_buffer,      max_num_particle_ * sizeof(float2));
-  cudaMalloc(&p2m_workingspace, base_size_ * base_size_*sizeof(int2));
+  cudaMalloc(&p2m_assigningIndex, base_size_ * base_size_*sizeof(int2));
   p2m_weighting<<<kNumBlock_pointwise,kNumThread_pointwise>>>(num_particle,gpuptr_position_,gpuptr_weight_,p2m_buffer);
 
   thrust::device_ptr<int2> thrust_index(gpuptr_index_);
   thrust::device_ptr<float2> thrust_position(gpuptr_position_);
   thrust::device_ptr<float> thrust_weight(gpuptr_weight_);
   thrust::device_ptr<float2> thrust_weighted(p2m_buffer);
-  thrust::device_ptr<int2> thrust_working(p2m_workingspace);
+  thrust::device_ptr<int2> thrust_assigninging(p2m_assigningIndex);
   thrust::device_ptr<float2> thrust_cellPos(gpuptr_cell_position_);
   thrust::device_ptr<float> thrust_cellWei(gpuptr_cell_weight_);
 
-  thrust::reduce_by_key(thrust_index, thrust_index + num_particle, thrust_weighted, thrust_working, thrust_cellPos);
-  thrust::reduce_by_key(thrust_index, thrust_index + num_particle, thrust_weight, thrust_working, thrust_cellWei);
-  p2m_averaging<<<kNumBlock_cellwise,kNumThread_cellwise>>>(base_size_,gpuptr_cell_position_,gpuptr_cell_weight_);
+  thrust::pair<int2*,float*> p2m_dummy;
+
+  thrust::reduce_by_key(thrust_index, thrust_index + num_particle, thrust_weighted, thrust_assigninging, thrust_cellPos+base_size_ * base_size_);
+  p2m_dummy=thrust::reduce_by_key(thrust_index, thrust_index + num_particle, thrust_weight, thrust_assigninging, thrust_cellWei+base_size_ * base_size_);
+
+  int assigning_length=p2m_dummy.second;
+  p2m_assigning<<<kNumBlock_cellwise,kNumThread_cellwise>>>(base_size_,gpuptr_cell_position_,gpuptr_cell_weight_,assigning_length,p2m_assigningIndex);
 
   cudaFree(p2m_buffer);
-  cudaFree(p2m_workingspace);
+  cudaFree(p2m_assigningIndex);
 }
 
 }  // namespace nbfmm
