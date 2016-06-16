bfc9870e1b883035c850d94da81c4ab8a7f12c34
Mu Yang <emfomy@gmail.com>
2016-06-16 17:37:07 +0800

    Fix typo.

--- source/nbfmm/model/circle.cu
+++ source/nbfmm/model/circle.cu
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/// @file    source/nbfmm/model/circle.hpp
+/// @file    source/nbfmm/model/circle.cu
 /// @brief   The implementation of circle shape generator.
 ///
 /// @author  Mu Yang <emfomy@gmail.com>
@@ -22,27 +22,27 @@ using namespace std;
 /// @param[in]   angle_difference   the difference between current angle and previous angle
 /// @param[out]  position_current   the device pointer of current particle positions.
 /// @param[out]  position_previous  the device pointer of previous particle positions.
-/// @param[out]  weight             the device pointer of particle weights.
+/// @param[out]  weight_current     the device pointer of particle weights.
 ///
 __global__ void generateModelCircleDevice(
-    const int     num_particles,
+    const int     num_particle,
     const float2  center_position,
     const float   radius,
     const float   weight,
     const float   angle_difference,
     float2*       position_current,
     float2*       position_previous,
-    float*        weight
+    float*        weight_current
 ) {
   const int idx = threadIdx.x + blockIdx.x * blockDim.x;
   if ( idx >= num_particle ) {
     return;
   }
-  const float  angle_current  = (2.0f * M_PI * idx) / num_particles;
+  const float  angle_current  = (2.0f * M_PI * idx) / num_particle;
   const float  angle_previous = angle_current - angle_difference;
   position_current[idx]       = make_float2(cosf(angle_current), sinf(angle_current));
   position_previous[idx]      = make_float2(cosf(angle_previous), sinf(angle_current));
-  weight[idx]                 = weight;
+  weight_current[idx]         = weight;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -52,29 +52,28 @@ namespace nbfmm {
 
 // Generate circle shape points
 void generateModelCircle(
-    const int     num_particles,
+    const int     num_particle,
     const float2  center_position,
     const float   radius,
     const float   weight,
     const float   tick,
     float2*       gpuptr_position_current,
     float2*       gpuptr_position_previous,
-    float*        gpuptr_weight
+    float*        gpuptr_weight_current
 ) {
-  assert( num_particles > 0 );
+  assert( num_particle > 0 );
   assert( radius > 0 );
   assert( weight > 0 );
 
   const int block_dim = kMaxBlockDim;
   const int grid_dim  = ((num_particle-1)/block_dim)+1;
 
-  const float2 effect = KernelFunction(make_float2(0.0f, 0.0f), make_float2(radius, 0.0f), weight * (num_particles-1));
+  const float2 effect = kernelFunction(make_float2(0.0f, 0.0f), make_float2(radius, 0.0f), weight * (num_particle-1));
   const float angle_difference = acos(1.0 - effect.x * tick * tick / radius / 2.0f);
 
   // Extract heads of cell index of each cell
-  generateModelCircleDevice<<<grid_dim, block_dim>>>(num_particles, center_position, radius, weight, angle_difference,
-                                                     gpuptr_position_current, gpuptr_position_previous,
-                                                     gpuptr_weight_current, gpuptr_weight_previous);
+  generateModelCircleDevice<<<grid_dim, block_dim>>>(num_particle, center_position, radius, weight, angle_difference,
+                                                     gpuptr_position_current, gpuptr_position_previous, gpuptr_weight_current);
 }
 
 }
